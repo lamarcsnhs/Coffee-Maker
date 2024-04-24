d09405d712dd270d1eaa2f00f269f42d8e9247b7
garrettboys <49699955+garrettboys@users.noreply.github.com>
2024-04-24 13:44:19 -0500

    added timeout to fix echo timing
    added a timeout of 0.01 seconds to improve consistency of sensor readouts and prevent wild inaccuracy. also mildly improved the constant value and readability

--- Sensor.py
+++ Sensor.py
@@ -6,7 +6,7 @@ trigger_pin_1 = 18
 echo_pin_2 = 12
 
 # Constant
-SPEED_OF_SOUND_CM_S = 34416 # speed of sound constant at 71f, just took the avg for best precision
+SPEED_OF_SOUND_CM_S = 34416 # speed of sound constant at 71 deg F, just took the avg for best precision
 
 # Inits GPIO unit for sensor
 def initGPIO(trg, echo):
@@ -14,7 +14,7 @@ def initGPIO(trg, echo):
     GPIO.setup(echo, GPIO.IN)
     GPIO.setup(trg, GPIO.OUT)
 
-def getDistance(trg, echo): # trigger/echo pins of sensor
+def getDistance(trg, echo, timeout = 0.01): # trigger/echo pins of sensor, timeout in sec
     # Sends a pulse to trigger the sensor
     GPIO.output(trg, True)
     time.sleep(0.00001)
@@ -23,16 +23,21 @@ def getDistance(trg, echo): # trigger/echo pins of sensor
     # Waits for the echo response
     pulse_start = time.time()
     pulse_end = time.time()
-
+    
     while GPIO.input(echo) == 0:
         pulse_start = time.time()
+        if pulse_start - start_time > timeout:
+            return None  # no echo received
 
+    pulse_end = pulse_start
     while GPIO.input(echo) == 1:
         pulse_end = time.time()
+        if pulse_end - pulse_start > timeout:
+            return None  # echo too long
 
     # Calculates pulse duration to get distance
     pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * SPEED_OF_SOUND_CM_S
+    distance = pulse_duration * SPEED_OF_SOUND_CM_S / 2 # division by two in order to get the one way distance time, as it gives the roundtrip without it
     distance = round(distance, 2)
 
     return distance
